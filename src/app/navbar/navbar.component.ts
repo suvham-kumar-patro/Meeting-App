c7fc6f389628dbd49c043f3c6671172e6810e682
Suvham Kumar Patro <suvham.patro@ascendion.com>
2024-12-17 00:45:47 +0530

    Updated changes in nav component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router'; 
 import { OnInit, OnDestroy } from '@angular/core';
 import { AuthenticationService } from '../common/auth/auth.service';
+import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
-  imports:[RouterLink, RouterLinkActive],
+  imports:[RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit, OnDestroy {
+  isLoggedIn: boolean = false;
   email: string = '';
   private emailSubscription: Subscription = new Subscription();
 
@@ -19,33 +21,23 @@ export class NavbarComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.emailSubscription = this.authService.currentUser.subscribe(user => {
       if (user) {
-        this.email=user.email;
+        this.isLoggedIn = true;  
+        this.email = user.email;  
       } else {
-        this.email='';
+        this.isLoggedIn = false; 
+        this.email = '';          
       }
     });
   }
 
-
-    // const storedEmail = this.authService.getEmail();
-    // this.email = storedEmail ? storedEmail : 'Guest'; 
-
-    // this.emailSubscription = this.authService.isLoggedIn$.subscribe((loggedIn) => {
-    //   if (loggedIn) {
-    //     this.email = this.authService.getEmail() || 'Guest';
-    //   } else {
-    //     this.email = 'Guest';
-    //   }
-    // });
-
   ngOnDestroy(): void {
     if (this.emailSubscription) {
-    this.emailSubscription.unsubscribe();
+      this.emailSubscription.unsubscribe();
+    }
   }
-}
 
   onLogout(): void {
-    this.authService.logout();
-    this.router.navigate(['/login']);
+    this.authService.logout(); 
+    this.router.navigate(['/login']);  
   }
 }
